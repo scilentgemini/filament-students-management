9dadc8fd5c75a6e912b16fe60bbaadec21c32c71
scilentgemini <rbchitthi@gmail.com>
2025-03-14 00:24:32 +0545

    student with dynamic section field CRUD done

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Models\Section;
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Student;
+use Filament\Forms\Get;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\StudentResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StudentResource\RelationManagers;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('class_id')
+                ->live()
+                ->relationship(name: 'class', titleAttribute: 'name'),
+
+                Select::make('section_id')
+                ->label('Section')
+                ->options(function(Get $get){
+                    $classId = $get('class_id');
+                    // info($classId);  //this logs to  storage->logs
+                    if($classId){
+                        return Section::where('class_id', $classId)->pluck('name', 'id')->toArray();
+                    }
+                }),
+
+                TextInput::make('name')
+                ->autofocus()
+                ->required(),
+
+                TextInput::make('email')
+                ->unique(ignoreRecord: true)
+                ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                ->searchable()
+                ->sortable(),
+
+                TextColumn::make('email')
+                ->searchable()
+                ->sortable(),
+
+                TextColumn::make('class.name')
+                ->badge()
+                ->searchable(),
+
+                TextColumn::make('section.name')
+                ->badge()
+                ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
