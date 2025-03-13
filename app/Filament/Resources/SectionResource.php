8f1866c9a77b04a7a1bc6f7558275f750143af47
scilentgemini <rbchitthi@gmail.com>
2025-03-13 23:20:33 +0545

    Section CRUD done

--- app/Filament/Resources/SectionResource.php
+++ app/Filament/Resources/SectionResource.php
@@ -4,20 +4,21 @@ namespace App\Filament\Resources;
 
 use Filament\Forms;
 use Filament\Tables;
-use App\Models\Classes;
+use App\Models\Section;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
-use App\Filament\Resources\ClassesResource\Pages;
+use App\Filament\Resources\SectionResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\ClassesResource\RelationManagers;
+use App\Filament\Resources\SectionResource\RelationManagers;
+use Filament\Tables\Columns\TextColumn;
 
-class ClassesResource extends Resource
+class SectionResource extends Resource
 {
-    protected static ?string $model = Classes::class;
+    protected static ?string $model = Section::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -25,6 +26,8 @@ class ClassesResource extends Resource
     {
         return $form
             ->schema([
+                Select::make('class_id')
+                    ->relationship(name: 'class', titleAttribute: 'name'),
                 TextInput::make('name'),
             ]);
     }
@@ -33,7 +36,9 @@ class ClassesResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name')
+                TextColumn::make('name'),
+                TextColumn::make('class.name')
+                ->badge(),
             ])
             ->filters([
                 //
@@ -59,9 +64,9 @@ class ClassesResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListClasses::route('/'),
-            'create' => Pages\CreateClasses::route('/create'),
-            'edit' => Pages\EditClasses::route('/{record}/edit'),
+            'index' => Pages\ListSections::route('/'),
+            'create' => Pages\CreateSection::route('/create'),
+            'edit' => Pages\EditSection::route('/{record}/edit'),
         ];
     }
 }
